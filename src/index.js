c1a55163badb02f79be0b691f6dd03e3e039fb10
Jam Risser <jam@jamrizzi.com>
2018-03-13 22:56:23 +0100

    Added license options

--- src/index.js
+++ src/index.js
@@ -92,9 +92,25 @@ export default class YoBasePrompts {
 
   async licensePrompt() {
     return this.optionOrPrompt({
-      type: 'input',
+      type: 'list',
       name: 'license',
       message: 'License:',
+      choices: [
+        { name: 'Apache 2.0', value: 'Apache-2.0' },
+        { name: 'MIT', value: 'MIT' },
+        { name: 'Mozilla Public License 2.0', value: 'MPL-2.0' },
+        {
+          name: 'BSD 2-Clause (FreeBSD) License',
+          value: 'BSD-2-Clause-FreeBSD'
+        },
+        { name: 'BSD 3-Clause (NewBSD) License', value: 'BSD-3-Clause' },
+        { name: 'Internet Systems Consortium (ISC) License', value: 'ISC' },
+        { name: 'GNU AGPL 3.0', value: 'AGPL-3.0' },
+        { name: 'GNU GPL 3.0', value: 'GPL-3.0' },
+        { name: 'GNU LGPL 3.0', value: 'LGPL-3.0' },
+        { name: 'Unlicense', value: 'unlicense' },
+        { name: 'No License (Copyrighted)', value: 'UNLICENSED' }
+      ],
       default: 'MIT'
     });
   }
